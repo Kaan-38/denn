888d3628138108ee26a48ff228312ca8591eb4c8
Kaan <kaansahin3800@gmail.com>
2022-11-09 21:03:43 +0300

    commit

--- src/Compoment/Navbar.jsx
+++ src/Compoment/Navbar.jsx
@@ -1,62 +1,87 @@
 import React from 'react'
 import './Navbar.css'
+import { Helmet } from 'react-helmet'
 const Navbar = ( { navbaropen } ) => {
   return (
     <div className={`ContainerNavbar ${navbaropen == true ? 'active' : ''} `}>
+
+      <Helmet>
+         <title>
+            DENGEBET
+         </title>
+      </Helmet>
        
         <div className='navbar-name'>
+            <a href="https://www.dengebet133.com/">
             <h1>
                 DENGEBET 
             </h1>
+            </a>
         </div>
         
         
         <li>
+            <a href="https://www.dengebet133.com/">
             <h3>
                 ANA SAYFA
             </h3>
+            </a>
         </li>
 
         <li>
+            <a href="https://www.dengebet133.com/">
             <h3>
                 CANLI BAHİS
             </h3>
+            </a>
         </li>
 
         <li>
+            <a href="https://www.dengebet133.com/">
             <h3>
                 CANLI CASİNO
             </h3>
+            </a>
         </li>
 
         <li>
+            <a href="https://www.dengebet133.com/">
             <h3>
                 SPOR BAHİSLERİ
             </h3>
+            </a>
         </li>
 
         <li>
+            <a href="https://www.dengebet133.com/">
             <h3>
                 E-SPORT
             </h3>
+            </a>
         </li>
 
         <li>
+            <a href="https://www.dengebet133.com/">
             <h3>
                 CASİNO
             </h3>
+            </a>
         </li>
 
         <li>
-            <h3>
+           <a href="https://www.dengebet133.com/">
+           <h3>
                 SANAL BAHİS
             </h3>
+           </a>
         </li>
 
         <li>
-            <h3>
+           <a href="https://www.dengebet133.com/">
+           <h3>
                 BAHİS BOZDUR
             </h3>
+           </a>
         </li>
     </div>
   )
--- src/Compoment/Promosyon.jsx
+++ src/Compoment/Promosyon.jsx
@@ -1,87 +1,134 @@
 import React from 'react'
 import './Promosyon.css'
+import { Helmet } from 'react-helmet'
 const Promosyon = () => {
   return (
   <div className='ContainerPromosyonKapsamOrtala'>
+
+      <Helmet>
+         <title>
+            DENGEBET
+         </title>
+      </Helmet>
         <div className='ContainerPromosyonKapsam'>
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/00a359e5275098b0c1fe063f53fb5586_1873833_news.png" alt="" />
+          <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/00a359e5275098b0c1fe063f53fb5586_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/38bba23e7daa23273bc98f4c127ea740_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/38bba23e7daa23273bc98f4c127ea740_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/410da74bea77303793920586f096037c_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/410da74bea77303793920586f096037c_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/84bffa33b37b101ceec200fc905c9db7_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/84bffa33b37b101ceec200fc905c9db7_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/d43b51c17ff6931dc997924b9a955f73_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/d43b51c17ff6931dc997924b9a955f73_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/e4e4ab783ab83788f28eef1c19214629_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/e4e4ab783ab83788f28eef1c19214629_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/07d59e7949ffad027228e8dad84ebc64_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/07d59e7949ffad027228e8dad84ebc64_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/d515027e1858b1f21a7f0a4dbb0e5443_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/d515027e1858b1f21a7f0a4dbb0e5443_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/93e746e979a882b7b59b6f7f2df6fb88_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/93e746e979a882b7b59b6f7f2df6fb88_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/514bd2986be3a6dec3918ca2adf81b24_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/514bd2986be3a6dec3918ca2adf81b24_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/8f61e5a4546b9c230a42f6a0adeebf02_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/8f61e5a4546b9c230a42f6a0adeebf02_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/a3924ec3eaf6fe1242dcbdf91f3990e9_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/a3924ec3eaf6fe1242dcbdf91f3990e9_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/414450e49bcb390416be1022a0bdfa15_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/414450e49bcb390416be1022a0bdfa15_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/a76f69794682f14f5f6c5edd94586c88_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/a76f69794682f14f5f6c5edd94586c88_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/6158b915fae32b61b64e92c314ec390e_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/6158b915fae32b61b64e92c314ec390e_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/e1f5ced9f6b7d86b058f221a7addb1b9_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/e1f5ced9f6b7d86b058f221a7addb1b9_1873833_news.png" alt="dengebet" /> 
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/582477cf9e3b35f337ee275ae84b0780_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/582477cf9e3b35f337ee275ae84b0780_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/25cf61fc073e203615f80cde92d784d0_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/25cf61fc073e203615f80cde92d784d0_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/08fd0afb849b922cec902af7c6373095_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/08fd0afb849b922cec902af7c6373095_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
 
        <div className='ContainerPromosyon'>
-          <img src="https://cmsbetconstruct.com/content/images/612e567e404f805d147167158c700f17_1873833_news.png" alt="" />
+       <a href="https://www.dengebet133.com/">
+          <img src="https://cmsbetconstruct.com/content/images/612e567e404f805d147167158c700f17_1873833_news.png" alt="dengebet" />
+          </a>
        </div>
     </div>
 </div>
